9d860162a531e78b77f46d6330a3406204ceb122
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-28 20:58:03 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Lesson12WINFROMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFamily = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AddName = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDlt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(748, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "שם";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(554, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "משפחה";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(391, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "גיל";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(241, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(518, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(626, 106);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtFamily
+            // 
+            this.txtFamily.Location = new System.Drawing.Point(448, 106);
+            this.txtFamily.Name = "txtFamily";
+            this.txtFamily.Size = new System.Drawing.Size(100, 20);
+            this.txtFamily.TabIndex = 6;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(274, 106);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(404, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 37);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "משתמשים";
+            // 
+            // AddName
+            // 
+            this.AddName.Location = new System.Drawing.Point(542, 155);
+            this.AddName.Name = "AddName";
+            this.AddName.Size = new System.Drawing.Size(75, 23);
+            this.AddName.TabIndex = 9;
+            this.AddName.Text = "הוספה";
+            this.AddName.UseVisualStyleBackColor = true;
+            this.AddName.Click += new System.EventHandler(this.AddName_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(124, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Id";
+            // 
+            // btnDlt
+            // 
+            this.btnDlt.Location = new System.Drawing.Point(433, 155);
+            this.btnDlt.Name = "btnDlt";
+            this.btnDlt.Size = new System.Drawing.Size(75, 23);
+            this.btnDlt.TabIndex = 13;
+            this.btnDlt.Text = "מחיקה";
+            this.btnDlt.UseVisualStyleBackColor = true;
+            this.btnDlt.Click += new System.EventHandler(this.btnDlt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.btnDlt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtFamily);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFamily;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button AddName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDlt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson12WINFROMS
+{
+    public partial class Form1 : Form
+    {
+        DataBaseEntities db = new DataBaseEntities();
+        Users users = new Users();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("error 11  " + ex.ToString());
+            }
+           
+
+        }
+
+        private void LoadData()
+        {
+            dataGridView1.DataSource = db.Users.ToList();
+        }
+
+        private void AddName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int.TryParse(txtAge.Text, out int age);
+                users.FirstName = txtName.Text;
+                users.LastName = txtFamily.Text;
+                users.Age = age;
+
+                db.Users.Add(users);
+                if(db.SaveChanges()>0)
+                {
+                    MessageBox.Show("success!");
+                    LoadData();
+                    
+                }
+
+
+            }
+            catch (Exception ex )
+            {
+                MessageBox.Show("error 234 " + ex.ToString());
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+
+            if(dataGridView1.CurrentCell.RowIndex != -1)
+            {
+                int userId = Convert.ToInt32(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells["Id"].Value);
+                users = db.Users.Where(x => x.Id == userId).FirstOrDefault();
+                if(users != null)
+                {
+                    txtName.Text = users.FirstName;
+                    txtFamily.Text = users.LastName;
+                    txtAge.Text = users.Age.ToString();
+                }
+            }
+        }
+
+        private void btnDlt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (users != null)
+                {
+                    db.Users.Remove(users);
+                    db.SaveChanges();
+                    MessageBox.Show("delete success!");
+                    LoadData();
+                }
+            }
+            catch (Exception ex )
+            {
+
+                MessageBox.Show("error delete! " + ex.ToString());
+            }
+           
+        }
+    }
+}
